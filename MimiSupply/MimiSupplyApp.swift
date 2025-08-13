05d181c9aa21a4939dd56726d3e962714bb3350c
MimiTechAi <info@mimitechai.com>
2025-08-13 14:20:29 +0200

    Initial Commit

---
+++ MimiSupply/MimiSupplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimiSupplyApp.swift
+//  MimiSupply
+//
+//  Created by Michael Bemler on 13.08.25.
+//
+
+import SwiftUI
+
+@main
+struct MimiSupplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
