1ca568de391d26b98a4eb93458dbe7b625c71fc9
MimiTechAi <info@mimitechai.com>
2025-08-21 22:54:07 +0200

    Added BlurView utility component for cross-platform glassmorphism effects and removed PremiumPartnerCard to simplify partner display system

---
+++ MimiSupply/DesignSystem/Components/Utility/BlurView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+/// Cross-platform BlurView for glassmorphism effects
+struct BlurView: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) { }
+}
